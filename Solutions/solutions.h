b6804baeb12b16a7d8895db2422bb70f16e2cde1
YU <silene26@Host-008.lan>
2025-09-23 22:55:37 +0200

    Implémentation de solutions.h pour le calcul de 1D-HO

--- Solutions/solutions.h
+++ Solutions/solutions.h
@@ -1,15 +1,29 @@
 #ifndef SOLUTIONS_H
 #define SOLUTIONS_H
 
+#include <armadillo>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
 
 
 class Solutions
 {
     public:
-    Solutions(int);
-    int h;
-    int m;
-    int w;
+    Solutions(double _m, double _h, double _w);
+    const double pi = 3.14;
+
+
+    arma::mat calc(int, arma::vec);
+
+    private:
+    // constante de Plank
+    double h=1.0;
+    // masse
+    double m=1.0;
+    // pulsation angulaire (angular frequence)
+    double w=1.0;
 
 };
 
