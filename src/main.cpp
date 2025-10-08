bc4be280f13e56c0a27b4c00987a37d332cccc80
YU <silene26@Host-008.lan>
2025-10-08 12:19:54 +0200

    Modification main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,9 @@ int main(int, char**)
 
     arma::vec z = arma::linspace(0,10,100);
     int n=5;
-    int m=solutions.factorielle(n);
-    auto hermite=solutions.hermite(n,z);
     auto result =solutions.calc(n,z);
     result.print("Calcul de l'oscillateur harmonique 1D en fonction de z pour un n donné");
-    hermite.print("Polynome d'Hermite d'axe z pour un n donné");
+
 
 
     
