85b39c54789e7dac928edf8877296d2927320ece
YU <silene26@Host-008.lan>
2025-10-10 14:17:45 +0200

    Mise à jour du test unitaire

--- tests/test_solutions.h
+++ tests/test_solutions.h
@@ -1,5 +1,5 @@
 #include <cxxtest/TestSuite.h>
-#include "../src/main.cpp"
+#include "../include/solutions.h"
 
 class TestSolutions: public CxxTest::TestSuite
 {
@@ -8,5 +8,15 @@ class TestSolutions: public CxxTest::TestSuite
     {
         Solutions solutions(1.0,1.0,1.0);
         TS_ASSERT_EQUALS(solutions.factorielle(5),120);
+        TS_ASSERT_EQUALS(solutions.factorielle(7),5040);
+        TS_TRACE("Test sur la fonction factorielle validé");
+    }
+
+    void testHermite(void)
+    {
+        arma::vec z =arma::linspace(0,10,100);
+        Solutions solutions(1.0,1.0,1.0);
+        TS_ASSERT_EQUALS(solutions.hermite(5,z),arma::ones(size(z))));
+
     }
 };
