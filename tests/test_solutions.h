3f28c329632ed9e2277deae2fbdef218270f7c3d
YU <silene26@Host-008.lan>
2025-10-21 17:20:12 +0200

    Mise à jour de test_solutions.h

--- tests/test_solutions.h
+++ tests/test_solutions.h
@@ -16,6 +16,28 @@ class TestSolutions: public CxxTest::TestSuite
         TS_ASSERT_EQUALS(solutions.factorielle(9),362880);
         
     }
+    // Vérifie si le polynome d'Hermite est correctement implémentée 
+    void testHermite(void)
+    {
+       Solutions solutions(1.0,1.0,1.0);
+       arma::vec x={-1,0,1,2};
+       // Vérification pour n=0
+       arma::vec H0=solutions.hermite(0,x);
+       arma::vec H0_expected=arma::ones(x.n_elem);
+       TS_ASSERT(arma::approx_equal(H0,H0_expected,"absdiff",1e-10));
+
+       // Vérification pour n=1
+       arma::vec H1=solutions.hermite(1,x);
+       arma::vec H1_expected=2*x;
+       TS_ASSERT(arma::approx_equal(H1,H1_expected,"absdiff",1e-10));
+
+       // Vérification pour n=1
+       arma::vec H2=solutions.hermite(1,x);
+       arma::vec H2_expected=4*pow(x,2)-2;
+       TS_ASSERT(arma::approx_equal(H1,H1_expected,"absdiff",1e-10));
+
+       
+    }
 
 
 };
